704fde88454a87be0994b7eda5819829073ce594
Lana <lana.sinapayen@gmail.com>
2021-06-25 18:31:49 +0900

    introduce multitone tone lib

--- sunchime.ino
+++ sunchime.ino
@@ -1,7 +1,4 @@
-/* Use a photoresistor (or photocell) to turn on an LED in the dark
-   More info and circuit schematic: http://www.ardumotive.com/how-to-use-a-photoresistor-en.html
-   Dev: Michalis Vasilakis // Date: 8/6/2015 // www.ardumotive.com */
-   
+#include <Tone.h>
 
 //Constants
 const int pResistor = A0; // Photoresistor at Arduino analog pin A0
@@ -22,12 +19,25 @@ int chime2 = 330;
 int chime3 = 392;
 int chime4 = 523; //c5
 
+
+Tone notePlayer[4];
+
 void setup(){
  pinMode(ledPin, OUTPUT);  // Set lepPin - 9 pin as an output
  pinMode(pResistor, INPUT);// Set pResistor - A0 pin as an input (optional)
 
  Serial.begin(9600);
  value = analogRead(pResistor);
+
+
+ notePlayer[0].begin(piezoPin);
+ notePlayer[1].begin(piezoPin);
+// notePlayer[2].begin(47);
+// notePlayer[3].begin(49);
+
+
+  //notePlayer[0].play(NOTE_C4,1000);
+  notePlayer[1].play(NOTE_D4,1000);
 }
 
 void energy_chime(int diff){
@@ -39,7 +49,7 @@ void energy_chime(int diff){
     diff = -diff;
   }
 
-  diff = 100*diff;
+  //diff = 10*diff;
   int max_tones = 6;
 
   // more and more time between chimes
@@ -47,22 +57,22 @@ void energy_chime(int diff){
     max_tones = max_tones-1;
     int actual_value = sign*diff;
     int rand_duration = random(30, chime_duration);
-    int rand_pause = random(10, pause_duration*max_tones);
+    int rand_pause = random(10, pause_duration*(6-max_tones));
 
     int energy_lost = random(50);
     diff = diff - energy_lost;
     
-    if (energy_lost>20){
-      tone(piezoPin, chime0, rand_duration);
-    } else if (energy_lost >15){
-      tone(piezoPin, chime1, rand_duration);
-    } else if (energy_lost >10){
-      tone(piezoPin, chime2, rand_duration);
-    } else if (energy_lost > 5){
-      tone(piezoPin, chime3, rand_duration);
-    } else if (energy_lost > 0){
-      tone(piezoPin, chime4, rand_duration);
-    } 
+//    if (energy_lost>20){
+//      tone(piezoPin, chime0, rand_duration);
+//    } else if (energy_lost >15){
+//      tone(piezoPin, chime1, rand_duration);
+//    } else if (energy_lost >10){
+//      tone(piezoPin, chime2, rand_duration);
+//    } else if (energy_lost > 5){
+//      tone(piezoPin, chime3, rand_duration);
+//    } else if (energy_lost > 0){
+//      tone(piezoPin, chime4, rand_duration);
+//    } 
     delay(rand_duration+rand_pause);
   }
   
@@ -79,34 +89,12 @@ void loop(){
   int difference = value - previous_value;
   Serial.print("difference ");
   Serial.println(difference);
-  if (abs(difference)>5){
-    energy_chime(difference);
-    delay(3000);
-  }
+//  if (abs(difference)>5){
+//    energy_chime(difference);
+//    delay(3000);
+//  }
 
-//  // piezo values between 20 and 20K
-//  if (value > 800){
-//    digitalWrite(ledPin, LOW);  //Turn led off
-//  }
-//  else if (value > 700){
-//    tone(piezoPin, 1500, 300);
-//    delay(300);
-//  }
-//  else if (value > 600){
-//    tone(piezoPin, 4000, 300);
-//    delay(300);
-//   }
-//  else if (value >500){ 
-//    digitalWrite(ledPin, HIGH); //Turn led on
-//
-//    /*Tone needs 2 arguments, but can take three
-//    1) Pin#
-//    2) Frequency - this is in hertz (cycles per second) which determines the pitch of the noise made
-//    3) Duration - how long teh tone plays
-//  */
-//    tone(piezoPin, 1000, 300);
-//    delay(300);
-//  }
+
 
   delay(500); //Small delay
 }
