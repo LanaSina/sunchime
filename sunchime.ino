37cdd3d2d581e4258fdaae66d128e7771bbf2ac5
Lana <lana.sinapayen@gmail.com>
2021-06-27 13:56:14 +0900

    working

--- sunchime.ino
+++ sunchime.ino
@@ -1,26 +1,15 @@
-#include <Tone.h>
+#include <PCM.h>
+#include "sounddata.h"
 
 //Constants
 const int pResistor = A0; // Photoresistor at Arduino analog pin A0
 const int ledPin=9;       // Led pin at Arduino pin 9
-int piezoPin = 12; //sound
+//int piezoPin = 12; //sound
 
 //Variables
 int value;          // Store value from photoresistor (0-1023)
 int previous_value;
 
-// sound in Hz (between 20 and 20K)
-// not possible to generate tones lower than 31Hz
-// max 65535
-// pentatonic scale, C, D, E, G, 
-int chime0 = 262; //c4
-int chime1 = 294;
-int chime2 = 330;
-int chime3 = 392;
-int chime4 = 523; //c5
-
-
-Tone notePlayer[4];
 
 void setup(){
  pinMode(ledPin, OUTPUT);  // Set lepPin - 9 pin as an output
@@ -29,56 +18,133 @@ void setup(){
  Serial.begin(9600);
  value = analogRead(pResistor);
 
-
- notePlayer[0].begin(piezoPin);
- notePlayer[1].begin(piezoPin);
-// notePlayer[2].begin(47);
-// notePlayer[3].begin(49);
-
-
-  //notePlayer[0].play(NOTE_C4,1000);
-  notePlayer[1].play(NOTE_D4,1000);
+  Serial.println(sizeof(chime0));
+  Serial.println(sizeof(chime1));
+  Serial.println(sizeof(chime2));
 }
 
-void energy_chime(int diff){
-  int chime_duration = 500;
-  int pause_duration = 100;
-  int sign = 1;
-  if (diff < 0){
-    sign = -1;
-    diff = -diff;
-  }
-
-  //diff = 10*diff;
-  int max_tones = 6;
-
-  // more and more time between chimes
-  while (diff>1 and max_tones>0){
-    max_tones = max_tones-1;
-    int actual_value = sign*diff;
-    int rand_duration = random(30, chime_duration);
-    int rand_pause = random(10, pause_duration*(6-max_tones));
-
-    int energy_lost = random(50);
-    diff = diff - energy_lost;
-    
-//    if (energy_lost>20){
-//      tone(piezoPin, chime0, rand_duration);
-//    } else if (energy_lost >15){
-//      tone(piezoPin, chime1, rand_duration);
-//    } else if (energy_lost >10){
-//      tone(piezoPin, chime2, rand_duration);
-//    } else if (energy_lost > 5){
-//      tone(piezoPin, chime3, rand_duration);
-//    } else if (energy_lost > 0){
-//      tone(piezoPin, chime4, rand_duration);
-//    } 
-    delay(rand_duration+rand_pause);
-  }
+// chime order could be generated during idle time 
+void generate_knock(int energy){
+   
+  Serial.println("generate ");
+  int n_chimes = 3;
+  int chime_length = 400; //ms, length of prestored chimes
   
+  // random durations between chime knocks
+  int knocks = 4;
+  int dist[] = {0,0,0,0};
+  int chime_seq[] = {-1,-1,-1,-1};
+  // can be truncated if same chime hit 2x
+  int truncated_durations[] = {chime_length,chime_length,chime_length,chime_length};
+  int max_duration = 0;
+  int sum_dist = 0;
+
+  // number of knocks
+  for (int i=0; i<knocks; i++){
+    //1st start will always be 0
+    if(i>0){
+        //min delay = 100 ms
+        int rand_dist = random(i*200) + 100;
+        sum_dist = sum_dist + rand_dist;
+        dist[i] = sum_dist;
+    }
+
+    chime_seq[i] = random(3);
+  }
+
+  //interrupt chime if it knocked 2x
+  for (int i=0; i<knocks; i++){
+    for (int j=i; j<4; j++){
+      if (j != i){
+        if (chime_seq[j] == chime_seq[i]){
+            truncated_durations[i] = min(chime_length, dist[j]-dist[i]);
+            //necessary to avoid bad overwrite
+            break;
+        }
+      }
+    }
+  }
+
+  max_duration = sum_dist+chime_length;
+
+  // now make the sound array
+  int sample_rate = 16000;
+  int num_samples = max_duration * (sample_rate / 1000.0);
+  unsigned char audio_int[num_samples] PROGMEM;
+  unsigned char audio_char[num_samples];
+  int chime_samples = chime_length*(sample_rate / 1000.0);
+  
+  int  max_volume = 0;
+  for (int x=0; x<num_samples; x++){
+      int value = 0;
+
+      for (int chime_n=0; chime_n<knocks; chime_n++){
+//        const unsigned char (*chime)[6400];
+//        if (chime_n == 0){
+//          chime = &chime0;
+//        } else if (chime_n == 1) {
+//          chime = &chime1;
+//        } else if (chime_n ==2){
+//          chime = &chime2;
+//        }
+      
+        int chime_start_time = dist[chime_n] * (sample_rate / 1000.0);
+        int chime_end_time = (dist[chime_n] + truncated_durations[chime_n]) * (sample_rate / 1000.0);
+  
+        // if this chime is on, add values to array
+        if(x > chime_start_time){
+          if (x < chime_end_time){
+            value = value + chime0[x]; //*chime[x];
+          }
+        }
+      }
+
+      if (value > max_volume){
+        max_volume = value;
+      }
+
+      //audio_int[x] = (unsigned char) 0;// value;
+  }
+
+
+  for (int x=0; x<num_samples; x++){
+    if (x<6400){
+      //chime0[x]; //(value/max_volume);
+      //audio_char[x] = chime0[x]; 
+      int a = 0;
+    }
+  }
+
+  startPlayback(chime1, 6400);  
+  Serial.println("played ");
 }
 
+void generate_knock_2(int energy){
+  int n_chimes = 3;
+  int knocks = 4;
+  int chime_length = 400; //ms, length of prestored chimes
+  int durations[] = {chime_length,chime_length,chime_length,chime_length};
+
+  // number of knocks
+  int min_delay = 410;
+  
+  for (int i=0; i<knocks; i++){
+    int chime_n = random(n_chimes);
+    if (chime_n == 0){
+      startPlayback(chime0, 6400);
+    } else if (chime_n == 1) {
+      startPlayback(chime1, 6400);
+    } else if (chime_n ==2){
+      startPlayback(chime2, 6400);
+    }  
+
+    int rand_dist = random(300) + i*150 ;
+    delay(rand_dist);
+  }
+
+}
 void loop(){
+
   previous_value = value;
   value = analogRead(pResistor);
   Serial.print("photocell value ");
@@ -89,12 +155,10 @@ void loop(){
   int difference = value - previous_value;
   Serial.print("difference ");
   Serial.println(difference);
-//  if (abs(difference)>5){
-//    energy_chime(difference);
-//    delay(3000);
-//  }
-
-
+  if (abs(difference)>10){
+    generate_knock_2(difference);
+    delay(2000);
+  }
 
   delay(500); //Small delay
 }
